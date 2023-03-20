2a3e289f4081ebc43862d8177bbbfe04bbfb65fe
PRCatarino <paulo061104@gmail.com>
2023-03-20 20:54:00 -0300

    'usandoLoop'

--- aula-3/dist/index.js
+++ aula-3/dist/index.js
@@ -5,28 +5,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const prompt_sync_1 = __importDefault(require("prompt-sync"));
 let prompt = (0, prompt_sync_1.default)();
+let next;
 let registeredCustomers = [];
 function register() {
-    function registered() {
-        let validCpf = prompt('Escreva seu CPF: ');
-        if (validCpf.length === 11) {
-            let myClients = {
-                Cpf: validCpf,
-                Name: prompt('Escreva seu nome: '),
-                Age: Number(prompt('Escreva seu idade: ')),
-                MaritalStatus: prompt('Escreva seu Estado Civil: '),
-                Address: prompt('Escreva sua endereço: '),
-                City: prompt('Escreva sua cidade: '),
-                State: prompt('Escreva seu estado: ')
-            };
-            registeredCustomers.push(myClients);
-            console.table(registeredCustomers);
+    let validCpf = prompt("Escreva seu CPF: ");
+    while (validCpf.length != 11) {
+        console.log(`O cpf ${validCpf} é inválido`);
+        next = prompt('deseja continuar: ');
+        if (next == 'sim') {
+            validCpf = prompt("Escreva seu CPF: ");
         }
         else {
-            console.log('cpf inválido');
-            register();
+            return;
         }
     }
-    registered();
+    let myClients = {
+        Cpf: validCpf,
+        Name: prompt("Escreva seu nome: "),
+        Age: Number(prompt("Escreva seu idade: ")),
+        MaritalStatus: prompt("Escreva seu Estado Civil: "),
+        Address: prompt("Escreva sua endereço: "),
+        City: prompt("Escreva sua cidade: "),
+        State: prompt("Escreva seu estado: "),
+    };
+    registeredCustomers.push(myClients);
+    console.table(registeredCustomers);
 }
 register();
--- aula-3/src/index.ts
+++ aula-3/src/index.ts
@@ -10,28 +10,29 @@ interface customers {
   City: string;
   State: string;
 }
+let next: string;
 let registeredCustomers: Array<customers> = [];
 function register() {
-  function registered() {
-    let validCpf: string = prompt("Escreva seu CPF: ");
-    
-    if (validCpf.length === 11) {
-      let myClients: customers = {
-        Cpf: validCpf,
-        Name: prompt("Escreva seu nome: "),
-        Age: Number(prompt("Escreva seu idade: ")),
-        MaritalStatus: prompt("Escreva seu Estado Civil: "),
-        Address: prompt("Escreva sua endereço: "),
-        City: prompt("Escreva sua cidade: "),
-        State: prompt("Escreva seu estado: "),
-      };
-      registeredCustomers.push(myClients);
-      console.table(registeredCustomers);
+  let validCpf: string = prompt("Escreva seu CPF: ");
+  while (validCpf.length != 11) {
+    console.log(`O cpf ${validCpf} é inválido`);
+    next = prompt("deseja continuar: ");
+    if (next == "sim") {
+      validCpf = prompt("Escreva seu CPF: ");
     } else {
-      console.log("cpf inválido");
-      register();
+      return;
     }
   }
-  registered();
+  let myClients: customers = {
+    Cpf: validCpf,
+    Name: prompt("Escreva seu nome: "),
+    Age: Number(prompt("Escreva seu idade: ")),
+    MaritalStatus: prompt("Escreva seu Estado Civil: "),
+    Address: prompt("Escreva sua endereço: "),
+    City: prompt("Escreva sua cidade: "),
+    State: prompt("Escreva seu estado: "),
+  };
+  registeredCustomers.push(myClients);
+  console.table(registeredCustomers);
 }
 register();
